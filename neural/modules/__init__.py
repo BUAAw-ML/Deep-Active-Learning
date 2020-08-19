ef4c4d1cf057da7e1eb27fe8ec5e75c6ee4fa378
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-08-19 15:50:58 +0800

    update

--- neural/modules/__init__.py
+++ neural/modules/__init__.py
@@ -1,8 +1,5 @@
 from .baseRNN import baseRNN
 from .EncoderRNN import EncoderRNN
-from .EncoderRNN_BB import EncoderRNN_BB
-
-
 from .EncoderCNN import EncoderCNN
-from .EncoderCNN_BB import EncoderCNN_BB
-from .EncoderCNN_Pair import EncoderCNN_Pair
+
+
